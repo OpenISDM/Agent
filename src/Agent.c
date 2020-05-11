f0dd1c315b085912e92ea998bc4d72ade7a7bceb
kangweicheng <b05505028@ntu.edu.tw>
2020-05-11 06:06:16 +0000

    add config files

--- src/Agent.c
+++ src/Agent.c
@@ -37,9 +37,48 @@
 #include "Agent.h"
 #include "zlog.h"
 
+ErrorCode get_config(Config *config, char *file_name) {
+    /* Return value is a struct containing all config information */
+    int retry_times = 0;
+    FILE *file = NULL;
+    char config_message[CONFIG_BUFFER_SIZE];
+    
+    /* open the config files */
+    retry_times = FILE_OPEN_RETRY;
+    while(retry_times--){
+        file = fopen(file_name, "r");
+
+        if(NULL != file){
+            break;
+        }
+    }
+
+    if (NULL == file) {
+        zlog_error(category_health_report,
+                   "Error openning file");
+        zlog_error(category_debug,
+                   "Error openning file");
+        return E_OPEN_FILE;
+    }
+
+    /* Keep reading each line and store into the config struct */
+
+    /* item 1 */
+    fetch_next_string(file, config_message, sizeof(config_message)); 
+    config -> agent_port = atoi(config_message);
+    
+    /* item 2 */
+    fetch_next_string(file, config_message, sizeof(config_message)); 
+    config -> light_controller_port = atoi(config_message);
+    
+    fclose(file);
+
+    return WORK_SUCCESSFULLY;
+}
+
 ErrorCode Wifi_init(){
     /* Initialize the Wifi cinfig file */
-    if(udp_initial(&udp_config, 9998)
+    if(udp_initial(&udp_config, g_config.agent_port)
                    != WORK_SUCCESSFULLY){
         /* Error handling TODO */
         return E_WIFI_INIT_FAIL;
@@ -56,13 +95,14 @@ void Wifi_free(){
 
 int main(int argc, char **argv) {
     ErrorCode return_value = WORK_SUCCESSFULLY;
+    ErrorCode config_value = get_config(&g_config, CONFIG_FILE_NAME);
     return_value = Wifi_init();
     if(return_value == WORK_SUCCESSFULLY){
       while(true){
         sPkt recv_queue = udp_getrecv(&udp_config);
         
         if(strlen(recv_queue. content)) {
-          udp_addpkt_without_encoding(&udp_config, "127.0.0.1", 9997, recv_queue.content, strlen(recv_queue.content));
+          udp_addpkt_without_encoding(&udp_config, "127.0.0.1", g_config.light_controller_port, recv_queue.content, strlen(recv_queue.content));
         }else{
           sleep_t(NORMAL_WAITING_TIME_IN_MS);
         }
--- src/Agent.h
+++ src/Agent.h
@@ -39,64 +39,11 @@ File Description:
 #include "BeDIS.h"
 #include "Version.h"
 
+#define CONFIG_FILE_NAME "../config/conf.conf"
 
 typedef struct Config {
-
-    /* String representation of the area id of the beacon location */
-    char area_id[CONFIG_BUFFER_SIZE];
-
-    /* String representation of the X coordinate of the beacon location */
-    char coordinate_X[CONFIG_BUFFER_SIZE];
-
-    /* String representation of the Y coordinate of the beacon location */
-    char coordinate_Y[CONFIG_BUFFER_SIZE];
-
-    /* String representation of the Z coordinate of the beacon location */
-    char coordinate_Z[CONFIG_BUFFER_SIZE];
-
-    /* The expected lowest basement under ground in the world. This constant
-    will be added to Z-coordinate (level information) gotten from input
-    configuration file. This adjustment helps us to have positive number in the
-    config data structure and lets Z-coordinate occupy only 2 bytes in UUID.
-    */
-    int lowest_basement_level;
-
-    /* String representation of the universally unique identifer */
-    char uuid[CONFIG_BUFFER_SIZE];
-
-    /* The dongle used to advertise */
-    int advertise_dongle_id;
-
-    /* Time interval in units of 0.625ms between advertising by a LBeacon */
-    int advertise_interval_in_units_0625_ms;
-
-    /* The rssi value used to advertise */
-    int advertise_rssi_value;
-
-    /* The dongle used to scan */
-    int scan_dongle_id;
-    
-    /* Time interval in units of 0.625ms between scanning by a LBeacon */
-    int scan_interval_in_units_0625_ms;
-
-    /* The required signal strength */
-    int scan_rssi_coverage;
-
-    /* The list of all acceptable mac address prefixes */
-    struct List_Entry mac_prefix_list_head;
-
-    /* The IPv4 network address of the gateway */
-    char gateway_addr[NETWORK_ADDR_LENGTH];
-
-    /* The UDP port of gateway connection*/
-    int gateway_port;
-
-    /* The IPv4 network address of LBeacon */
-    char local_addr[NETWORK_ADDR_LENGTH];
-
-    /* The UDP port for LBeacon to listen and receive UDP from gateway*/
-    int local_client_port;
-
+    int agent_port;
+    int light_controller_port;
 } Config;
 
 /*
