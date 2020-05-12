ddf046e8d64835cb1d8183a06a56fb9b864f3066
kangweicheng <b05505028@ntu.edu.tw>
2020-05-12 22:47:57 +0800

    introduce sigint

--- bin/auto_Agent.sh
+++ bin/auto_Agent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo python LightControl.py &
 sudo ./Agent >/dev/null 2>&1 &
 
 exit 0
--- bin/kill_Agent.sh
+++ bin/kill_Agent.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# |: the output of left side is passed to the right side as input
+# ps -ef: get all environmental parameters
+# grep Agent: get the process with name "Agent"
+# grep -v grep: this filter out the row with "grep"
+# awk '{print $2}': print the second column, which is PID
+# xargs sudo kill -SIGINT: kill the process by PID
+
+
+
+
 ps -ef | grep Agent | grep -v grep | awk '{print $2}' | xargs sudo kill -SIGINT
