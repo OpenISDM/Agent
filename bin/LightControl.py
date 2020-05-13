d1ad80b500abb710e4473f7b5db777856d528620
Jimmy Lai <chunyu1202@gmail.com>
2020-05-13 16:56:00 +0800

    Uncomment the code.

--- bin/LightControl.py
+++ bin/LightControl.py
@@ -1,9 +1,9 @@
 import serial
 import socket
 import time
-FROM_GATEWAY 	= 0
+FROM_SOURCE_TYPE 	= 0
 FIRE_ALARM 		= 1
-API_VERSION 	= 2
+API_VERSION 	        = 2
 MODE 			= 3
 DURATION 		= 4
 def get_host_ip():
@@ -30,10 +30,10 @@ def alert_operation(mode, duration):
 	else: 
 		print('unrecognized mode')
 
-# setup serial port
-#conn = serial.Serial('/dev/ttyUSB0',timeout=.2)
+#setup serial port
+conn = serial.Serial('/dev/ttyUSB0',timeout=.2)
 time.sleep(3)
-##conn.write('FQ')
+conn.write('FQ')
 
 # setup UDP client port
 IP = get_host_ip()
@@ -46,21 +46,21 @@ print('Start UDP Server Listening at port: {}'.format(IP))
 while True:
 	bytesAddressPair = sock.recvfrom(bufferSize)
 	print(bytesAddressPair[0], bytesAddressPair[1])
-	# print(bytesAddressPair[0], bytesAddressPair[1])
-	#message = bytesAddressPair[0]
-	#address = bytesAddressPair[1]
-	#API_ver = None
-	#print('received message: {}, from: {}'.format(message, address))
-	#splitted_message = message.split(';')
-	#if(splitted_message[FROM_GATEWAY] != '6'):
-	#	print('Data aren\'t from Gateway')
-	#	continue
-	#elif(splitted_message[FIRE_ALARM] != '7'):
-	#	print('FIRE_ALARM Error')
-	#	continue
-#	else:
-	#	API_ver = splitted_message[API_VERSION]
-		#mode = int(splitted_message[MODE])
-	#	duration = int(splitted_message[DURATION])
-	#	alert_operation(mode, duration)
-	# time.sleep(0.3)
+	print(bytesAddressPair[0], bytesAddressPair[1])
+	message = bytesAddressPair[0]
+	address = bytesAddressPair[1]
+	API_ver = None
+	print('received message: {}, from: {}'.format(message, address))
+	splitted_message = message.split(';')
+	if(splitted_message[FROM_SOURCE_TYPE] != '2'):
+		print('Data aren\'t from Server')
+		continue
+	elif(splitted_message[FIRE_ALARM] != '7'):
+		print('FIRE_ALARM Error')
+		continue
+	else:
+		API_ver = splitted_message[API_VERSION]
+                mode = int(splitted_message[MODE])
+		duration = int(splitted_message[DURATION])
+		alert_operation(mode, duration)
+	        time.sleep(0.3)
